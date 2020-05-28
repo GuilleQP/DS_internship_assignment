fca76cd2dd4d0ef960f5a22ed7cbdb7015eb8cf9
Guillermo Quintana Pelayo <36505071+GuilleQP@users.noreply.github.com>
2020-05-28 16:30:33 +0200

    Collected data results

--- src/scrap.py
+++ src/scrap.py
@@ -6,7 +6,6 @@ Web scrapper
 '''
 import requests
 import pandas as pd
-import os
 import json
 from bs4 import BeautifulSoup
 
@@ -15,7 +14,7 @@ url = 'https://api.ycombinator.com/companies/export.json?callback=true'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37'}
 # Obtain page as html
 page = requests.get(url,timeout=(3.05, 27),headers=headers)
-# Transform html to a readableformat
+# Transform html to a readable format
 soup = BeautifulSoup(page.content, 'html.parser')
 # Parse text to remove unwanted formatting
 text = soup.text
